0f4341645f53778cb9896a7427f2bfad35dc9a5f
Davyd McColl <davydm@gmail.com>
2022-08-18 15:37:18 +0200

    :bug: providing any value for 'inherit' wipes out inherited attributes

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+using System.Reflection;
+
+var prop = typeof(DerivedType).GetProperty(nameof(DerivedType.Id));
+var unspecified = prop.GetCustomAttributes().Select(a => a.GetType().Name).ToArray();
+var inheritTrue = prop.GetCustomAttributes(true).Select(a => a.GetType().Name).ToArray();
+var inheritFalse = prop.GetCustomAttributes(false).Select(a => a.GetType().Name).ToArray();
+
+Console.WriteLine($"default: {string.Join(",", unspecified)}");
+Console.WriteLine($"inherit == true: {string.Join(",", inheritTrue)}");
+Console.WriteLine($"inherit == false: {string.Join(",", inheritFalse)}");
+
+public class BaseType
+{
+    [BaseProperty]
+    public virtual int Id { get; set; }
+}
+
+public class DerivedType : BaseType
+{
+    [DerivedProperty]
+    public override int Id { get; set; }
+}
+
+public class BasePropertyAttribute : Attribute
+{
+}
+public class DerivedPropertyAttribute : Attribute
+{
+}
